06c7d68aeafcf833b9631b1acd20e0d082ff1da1
guimeiling <guimeiling@njydsz.com>
2023-12-11 16:25:26 +0800

    feat:markdown右侧功能完善

--- cms-web-ui/src/components/Markdown/src/Markdown.vue
+++ cms-web-ui/src/components/Markdown/src/Markdown.vue
@@ -16,15 +16,12 @@
   import Vditor from 'vditor';
   import 'vditor/dist/index.css';
   import { useLocale } from '/@/locales/useLocale';
-import { useModalContext } from '../../Modal';
-  import { toolbar } from './toolbar'
+  import { useModalContext } from '../../Modal';
+  import { toolbar,liuchengtu } from './toolbar';
   import { useRootSetting } from '/@/hooks/setting/useRootSetting';
-import { onMountedOrActivated } from '/@/hooks/core/onMountedOrActivated';
-import { getAppEnvConfig } from '/@/utils/env';
-import { GetUploadLink,Upload } from '/@/api/sys/file'
-const {
-  VITE_GLOB_UPLOAD_URL,
-  } = getAppEnvConfig();
+  import { onMountedOrActivated } from '/@/hooks/core/onMountedOrActivated';
+  import { GetUploadLink,Upload } from '/@/api/sys/file';
+  import { svgPath } from './svg';
   import { getTheme } from './getTheme';
 
   type Lang = 'zh_CN' | 'en_US' | 'ja_JP' | 'ko_KR' | undefined;
@@ -108,29 +105,141 @@ const {
           },
           upload: {
             accept: 'image/*',
-            // url: VITE_GLOB_UPLOAD_URL,
-            // linkToImgUrl: VITE_GLOB_UPLOAD_URL,
-            headers: {
-              'X-SERVICE-TYPE': 'webService',
-              'X-GROUP-TENANT-ID':'1667075454562709506',
-              'X-COMPANY-TENANT-ID': '1667075454562709506',
-            },
             fieldName: 'multipartFile',
-            handler:  (files) => {
-              const array = []
-              files.forEach(file => {
-                array.push(Upload({ file:file }))
-              })
-              Promise.all(array).then((list) => { 
-                console.log('listlist',list);
+            handler: (files) => {
+              const array = [];
+              files.forEach((file) => {
+                array.push(Upload({ file: file }));
+              });
+              Promise.all(array).then((list) => {
+                console.log('listlist', list);
                 list.forEach(async (item) => {
-                  let res = await GetUploadLink({ expires: 30000, objectName: item.data.data.fileUrl })
-                  insEditor?.insertValue(`![${item.data.data.fileName}](${res})<br/>`); // 文本编辑器中插入图片
-                })
-              })
+                  let res = await GetUploadLink({
+                    expires: 30000,
+                    objectName: item.data.data.fileUrl,
+                  });
+                  insEditor?.insertValue(`<p data-block="0">\u200b<wbr>\n![${item.data.data.fileName}](${res})</p>`, true); // 文本编辑器中插入图片
+                });
+              });
             },
           },
-          toolbar,
+          toolbar: [
+            ...toolbar,
+            // {
+            //   name: 'alignType',
+            //   icon: svgPath.alignType,
+            //   tip: '对齐方式',
+            //   tipPosition: 'ne',
+            //   toolbar: [
+            //     {
+            //       name: 'alignType-left',
+            //       icon: svgPath.alignTypeLeft + '&nbsp;&nbsp;左对齐',
+            //       click: (event: Event, vditor: IVditor) => {
+            //         // setAlign('left')
+            //       }
+            //     },
+            //     {
+            //       name: 'alignType-center',
+            //       icon: svgPath.alignType + '&nbsp;&nbsp;居中对齐',
+            //       click: (event: Event, vditor: IVditor) => {
+            //         // setAlign('center')
+            //       }
+            //     },
+            //     {
+            //       name: 'alignType-right',
+            //       icon: svgPath.alignTypeRight + '&nbsp;&nbsp;右对齐',
+            //       click: (event: Event, vditor: IVditor) => {
+            //         // setAlign('right')
+            //       }
+            //     }
+            //   ]
+            // },
+            {
+              name: 'liuchengtu',
+              icon: svgPath.liuchengtu,
+              tip: '图表',
+              tipPosition: 'ne',
+              toolbar: liuchengtu.map(item => {
+                return {
+                  ...item,
+                  click: (event: Event, vditor: IVditor) => {
+                    insEditor?.insertValue(item.textValue,true)
+                  }
+                }
+              })
+            },
+            {
+              name: 'fullscreen',
+              className:'toolbar-right'
+            },
+            {
+              name: 'help',
+              className:'toolbar-right'
+            },
+            {
+              name: 'summary',
+              icon: svgPath.summary,
+              tip: '目录',
+              tipPosition: 'ne',
+              className:'toolbar-right',
+              click: (event: Event, vditor: IVditor) => {
+                const btnEllastElementChildement = vditor.toolbar.elements.summary?.lastElementChild;
+                btnEllastElementChildement.ariaLabel = `目录`
+                if (vditor.outline.element.style.display == 'none') {
+                  vditor.outline.element.style.display = "block"
+                  btnEllastElementChildement.ariaLabel = `关闭目录`
+                } else {
+                  vditor.outline.element.style.display = "none"
+                  btnEllastElementChildement.ariaLabel = `目录`
+                }
+              }
+            },
+            {
+              name: 'previewCard',
+              icon: svgPath.previewCard,
+              tip: '仅预览区',
+              tipPosition: 'ne',
+              className: 'toolbar-right',
+              click: (event: Event, vditor: IVditor) => {
+                const editCard = vditor.toolbar.elements.editCard
+                const previewCard = vditor.toolbar.elements.editCard.lastElementChild
+                editCard.lastElementChild.ariaLabel = `仅编辑区`
+                if (vditor.sv.element.style.display == 'none') {
+                  vditor.sv.element.style.display = "block"
+                  vditor.preview.element.style.display = "block";
+                  previewCard.ariaLabel = `仅预览区`
+                } else {
+                  vditor.sv.element.style.display = "none"
+                  vditor.preview.element.style.display = "block";
+                  previewCard.ariaLabel = `恢复默认`
+                }
+                // insEditor?.setPreviewMode('editor')
+              }
+            },
+            {
+              name: 'editCard',
+              tip: `仅编辑区`,
+              icon: svgPath.editCard,
+              tipPosition: 'ne',
+              className: 'toolbar-right',
+              click: (event: Event, vditor: IVditor) => {
+                const previewCard = vditor.toolbar.elements.previewCard
+                const editCard = vditor.toolbar.elements.editCard.lastElementChild
+                previewCard.lastElementChild.ariaLabel = `仅预览区`
+                if (vditor.options.preview.mode == 'editor') {
+                  insEditor?.setPreviewMode('both')
+                  vditor.sv.element.style.display = "block"
+                  vditor.preview.element.style.display = "block";
+                  editCard.ariaLabel = `仅编辑区`
+                } else {
+                  insEditor?.setPreviewMode('editor')
+                  vditor.sv.element.style.display = "block"
+                  vditor.preview.element.style.display = "none";
+                  editCard.ariaLabel = `恢复默认`
+                }
+              }
+            },
+          ],
           preview: {
             theme: {
               // 设置内容主题
@@ -194,7 +303,17 @@ const {
 <style lang="less">
   .vditor-toolbar {
     .vditor-toolbar__item {
-      margin:0.1rem .4rem !important;
+      margin: 0.1rem 0.4rem !important;
+    }
+    .toolbar-right {
+      float:right
+    }
+  }
+  .right {
+    .vditor-tooltipped {
+      &::after {
+        display: none;
+      }
     }
   }
 </style>
